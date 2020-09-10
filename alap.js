9fc2513b2a6b69c97413a52d4bb16eba8d80c3a5
borcsokdavid <borcsokdavid@gmail.hu>
2020-09-10 09:27:13 +0200

    Harmadik kommit

---
+++ alap.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//Adat típusok: number, bigint, string, boolean, null, undefined, object, symbol
+
+let nev = 'Börcsök Dávid';
+
+
+
+/* Modális ablak függvények */
+// window.alert(nev);
+// alert(nev);
+
+// let tanuloSzam = window.prompt('Hány tanuló van jelen:', 12);
+// let szoveg = `A tanulók száma: ${tanuloSzam}`;
+
+
+let szoveg = 'Hello world';
+//szoveg = null;
+//window.alert(szoveg);
+
+document.getElementById('kiir').innerText = szoveg;
